f5492bcf099fb2cbc6b3a1e468101711ae754275
Akshay <akshaykashyap0545@gmail.com>
2025-08-17 15:49:43 +0530

    b

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI ;
+  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/MANGODESK';
   try {
     await mongoose.connect(uri, {
       dbName: process.env.MONGO_DB_NAME || 'MANGODESK',
