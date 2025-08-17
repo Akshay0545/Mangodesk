f2d5da8d8469664a18d46292431a1801b0b6f30b
Akshay <akshaykashyap0545@gmail.com>
2025-08-17 15:51:32 +0530

    c

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://summarizerapi-h9tg.onrender.com' || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://summarizerapi-h9tg.onrender.com/api' || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
